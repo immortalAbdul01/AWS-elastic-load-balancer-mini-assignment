7ebe7010b1f52f3b7344abe69976f68b7250e967
Immortal <ak2061435@gmail.com>
2023-11-28 15:43:31 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+
+app.use(bodyParser.json());
+
+
+app.post('/api/hello', (req, res) => {
+   
+    
+    
+
+    res.json({ message: "Hello World"});
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
